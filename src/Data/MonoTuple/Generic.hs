64be36bbc7be83240641a837cb22006aff9903f2
Taras Serduke <taras.serduke@gmail.com>
2016-08-29 14:01:01 +0300

    #12 generic

--- src/Data/MonoTuple/Generic.hs
+++ src/Data/MonoTuple/Generic.hs
@@ -1,2 +1,13 @@
 module Data.MonoTuple.Generic where
 
+import Data.Data (Data, fromConstr, gmapQi, toConstr)
+
+
+{-# INLINE elementAt #-}
+elementAt :: (Data a, Data b) => a -> Int -> b
+elementAt x i = gmapQi i recast x
+
+
+{-# INLINE recast #-}
+recast :: (Data a, Data b) => a -> b
+recast = fromConstr . toConstr
