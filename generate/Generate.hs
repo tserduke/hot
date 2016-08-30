0727356588bc5917fc8f0014a080a22edbf7c0a7
Taras Serduke <taras.serduke@gmail.com>
2016-08-31 00:32:47 +0300

    #17 unlines refactoring

--- generate/Generate.hs
+++ generate/Generate.hs
@@ -1,4 +1,4 @@
-module Main where
+module Main (main) where
 
 import BasicPrelude
 
@@ -18,43 +18,43 @@ baseModule n = runLines $ do
   "\n"
   "class (Hot t n, Data (t a), Data a) => HotData t a n"
   ""
-  Line $ unlinesN n instanceHotData
+  forN n instanceHotData
   "\n"
   "class Hot (t :: * -> *) (n :: Nat) | t -> n, n -> t where"
   tab 1 "size :: t a -> Int"
   tab 1 "mapAt :: (a -> a) -> t a -> Int -> t a"
   "\n"
-  Line $ unlinesN1 n instanceHot
-  "\n"
-  Line $ unlinesN1 n dataHot
+  forN n instanceHot
   ""
+  forN n dataHot
 
 
-instanceHotData, instanceHot, dataHot :: Int -> Text
-instanceHotData n = "instance (Hot Hot" ++ show n +++ "n, Data a) => HotData Hot" ++ show n +++ "a n"
+instanceHotData, instanceHot, dataHot :: Int -> Line ()
+instanceHotData n = Line $ "instance (Hot Hot" ++ show n +++ "n, Data a) => HotData Hot" ++ show n +++ "a n"
 
-instanceHot n = runLines $ do
+instanceHot n = do
   Line $ "instance Hot Hot" ++ show n +++ show n +++ "where"
   tab 1 $ "size _ =" +++ show n
   tab 1 $ "mapAt f (" ++ hotConstr n (("x" ++) . show) ++ ") = \\case"
-  Line $ unlinesN n (mapAtCase n)
+  forN n (mapAtCase n)
   tab 2 $ "n -> error $ \"Hot" ++ show n ++ " mapAt \" ++ show n"
+  ""
 
-mapAtCase :: Int -> Int -> Text
-mapAtCase n i = "\t\t" ++ show (i - 1) +++ "->" +++ hotConstr n f where
+mapAtCase :: Int -> Int -> Line ()
+mapAtCase n i = tab 2 $ show (i - 1) +++ "->" +++ hotConstr n f where
   f j = if j == i then "(f x" ++ show j ++ ")" else "x" ++ show j
 
-dataHot n = runLines $ do
+dataHot n = do
   Line $ "data Hot" ++ show n +++ "a =" +++ hotConstr n (const "!a")
   tab 1 $ "deriving (Eq, Ord, Read, Show, Data, Typeable)"
+  ""
 
 hotConstr :: Int -> (Int -> Text) -> Text
 hotConstr n f = "T" ++ show n +++ unwords (map f [1 .. n])
 
 
-unlinesN, unlinesN1 :: Int -> (Int -> Text) -> Text
-unlinesN n f = intercalate "\n" $ map f [1 .. n]
-unlinesN1 n f = intercalate "\n\n" $ map f [1 .. n]
+forN :: (Monad m) => Int -> (Int -> m a) -> m a
+forN n f = foldr1 (>>) $ map f [1 .. n]
 
 (+++) :: Text -> Text -> Text
 x +++ y = x ++ " " ++ y
