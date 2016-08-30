29389c80aa6b5d509297248d38f01dc1142ffc6c
Taras Serduke <taras.serduke@gmail.com>
2016-08-31 01:15:26 +0300

    #12 no test needed

--- bench/Data/HotBench.hs
+++ bench/Data/HotBench.hs
@@ -8,11 +8,7 @@ import Data.Hot
 
 benchHot :: Benchmark
 benchHot = bgroup "Hot"
-  [ bgroup "elementAt"
-    [ bench "impl" $ whnf (sum . map (elementAt t10)) [0 .. 9]
-    , bench "matching" $ whnf (sum . map (elementAt10 t10)) [0 .. 9]
-    ]
-  , bgroup "merge"
+  [ bgroup "merge"
     [ bgroup "5"
       [ bench "merge" $ whnf (merge (Hot2 1 3)) (Hot3 2 4 6 :: Hot3 Int)
       , bench "list" $ nf (L.merge [1, 3]) ([2, 4, 6] :: [Int])
@@ -29,21 +25,3 @@ benchHot = bgroup "Hot"
       ]
     ]
   ]
-
-t10 :: Hot10 Int
-t10 = Hot10 1 2 3 4 5 6 7 8 9 10
-
-
-elementAt10 :: Hot10 Int -> Int -> Int
-elementAt10 (Hot10 x1 x2 x3 x4 x5 x6 x7 x8 x9 x10) = \case
-  0 -> x1
-  1 -> x2
-  2 -> x3
-  3 -> x4
-  4 -> x5
-  5 -> x6
-  6 -> x7
-  7 -> x8
-  8 -> x9
-  9 -> x10
-  n -> error $ show n
