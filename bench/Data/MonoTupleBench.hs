be960ff74766a28b2442963abfddc06e747a2a82
Taras Serduke <taras.serduke@gmail.com>
2016-08-30 22:34:57 +0300

    #24 list benchmark

--- bench/Data/MonoTupleBench.hs
+++ bench/Data/MonoTupleBench.hs
@@ -2,7 +2,7 @@ module Data.MonoTupleBench (benchTuple) where
 
 import Criterion
 
-import qualified Data.List.Extra as E
+import qualified Data.List.Extra as L
 import Data.MonoTuple
 
 
@@ -14,16 +14,20 @@ benchTuple = bgroup "MonoTuple"
     ]
   , bgroup "merge"
     [ bgroup "5"
-      [ bench "merge" $ whnf (merge (T2 (1 :: Int) 3)) (T3 2 4 6)
-      , bench "list" $ nf (E.merge [(1 :: Int), 3]) [2, 4, 6]
+      [ bench "merge" $ whnf (merge (T2 1 3)) (T3 2 4 6 :: Tuple3 Int)
+      , bench "list" $ nf (L.merge [1, 3]) ([2, 4, 6] :: [Int])
       ]
     , bgroup "10"
-      [ bench "merge" $ whnf (merge (T4 (1 :: Int) 3 5 7)) (T6 2 4 6 8 10 12)
-      , bench "stream" $ whnf (last . E.merge (enumFromThenTo (1 :: Int) 3 7)) (enumFromThenTo 2 4 12)
-      , bench "list" $ nf (E.merge [(1 :: Int), 3, 5, 7]) [2, 4, 6, 8, 10, 12]
+      [ bench "merge" $ whnf (merge (T4 1 3 5 7)) (T6 2 4 6 8 10 12 :: Tuple6 Int)
+      , bench "stream" $ whnf (last . L.merge (enumFromThenTo 1 3 7)) (enumFromThenTo 2 4 12 :: [Int])
+      , bench "list" $ nf (L.merge [1, 3, 5, 7]) ([2, 4, 6, 8, 10, 12] :: [Int])
       ]
     ]
-
+  , bgroup "sort"
+    [ bgroup "10"
+      [ bench "list" $ nf L.sort ([2, 9, 5, 7, 2, 10, 7, 4, 3, 1] :: [Int])
+      ]
+    ]
   ]
 
 t10 :: Tuple10 Int
