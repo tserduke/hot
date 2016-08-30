b70bb78d77b04b5b898a1c6a4f980a2426a5646a
Taras Serduke <taras.serduke@gmail.com>
2016-08-30 11:24:22 +0300

    #11 benchmarks

--- bench/Data/MonoTupleBench.hs
+++ bench/Data/MonoTupleBench.hs
@@ -2,6 +2,7 @@ module Data.MonoTupleBench (benchTuple) where
 
 import Criterion
 
+import qualified Data.List.Extra as E
 import Data.MonoTuple
 
 
@@ -11,6 +12,18 @@ benchTuple = bgroup "MonoTuple"
     [ bench "impl" $ whnf (sum . map (elementAt t10)) [0 .. 9]
     , bench "matching" $ whnf (sum . map (elementAt10 t10)) [0 .. 9]
     ]
+  , bgroup "merge"
+    [ bgroup "5"
+      [ bench "merge" $ whnf (merge (T2 (1 :: Int) 3)) (T3 2 4 6)
+      , bench "list" $ nf (E.merge [(1 :: Int), 3]) [2, 4, 6]
+      ]
+    , bgroup "10"
+      [ bench "merge" $ whnf (merge (T4 (1 :: Int) 3 5 7)) (T6 2 4 6 8 10 12)
+      , bench "stream" $ whnf (last . E.merge (enumFromThenTo (1 :: Int) 3 7)) (enumFromThenTo 2 4 12)
+      , bench "list" $ nf (E.merge [(1 :: Int), 3, 5, 7]) [2, 4, 6, 8, 10, 12]
+      ]
+    ]
+
   ]
 
 t10 :: Tuple10 Int
