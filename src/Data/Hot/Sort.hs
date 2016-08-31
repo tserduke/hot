23ae813de59c3741b0e59bb758c42dd55015c52a
Taras Serduke <taras.serduke@gmail.com>
2016-09-01 01:45:38 +0300

    #24

--- src/Data/Hot/Sort.hs
+++ src/Data/Hot/Sort.hs
@@ -15,19 +15,20 @@ class (Hot t (HotNat t)) => Sort t where
   sort :: (Ord a) => t a -> t a
 
 
-instance Sort Hot1 where
+instance {-# OVERLAPPING #-} Sort Hot1 where
   {-# INLINE sort #-}
   sort = id
 
-instance Sort Hot2 where
+instance {-# OVERLAPPING #-} Sort Hot2 where
   {-# INLINABLE sort #-}
   sort t@(Hot2 x y) = if x > y
     then Hot2 y x
     else t
 
-instance (Hot t n, n ~ HotNat t, n ~ (n1 + n2),
-          Hot t1 n1, Sort t1, t1 ~ HotType n1, n1 ~ Half n, n1 <= n,
-          Hot t2 n2, Sort t2, t2 ~ HotType n2, n2 ~ (n - n1), n2 <= n) => Sort t where
+instance {-# OVERLAPPABLE #-} (
+         Hot t n, n ~ HotNat t, n ~ (n1 + n2),
+         Hot t1 n1, Sort t1, t1 ~ HotType n1, n1 ~ Half n, n1 <= n,
+         Hot t2 n2, Sort t2, t2 ~ HotType n2, n2 ~ (n - n1), n2 <= n) => Sort t where
   {-# INLINABLE sort #-}
   sort :: forall a. (Ord a) => t a -> t a
   sort t = merge (sort $ prefix t :: t1 a) (sort $ suffix t :: t2 a)
