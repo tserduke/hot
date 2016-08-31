10c7cb9b5d1ed3a4f942c7a3d4748400f13e43b5
Taras Serduke <taras.serduke@gmail.com>
2016-08-31 10:32:33 +0300

    #20

--- src/Data/Hot/Generic.hs
+++ src/Data/Hot/Generic.hs
@@ -1,13 +1,28 @@
 module Data.Hot.Generic
-  ( merge
+  ( prefix
+  , suffix
+  , merge
   ) where
 
 import Data.Hot.Base
 import GHC.TypeLits
 
 
+prefix, suffix :: forall t n t1 m a. (Hot t n, Hot t1 m) => t a -> t1 a
+
+prefix t = runSub $ unfold f (Sub 0) where
+  f (Sub i k) = Sub (i + 1) (k (elementAt t i))
+
+suffix t = runSub $ unfold f (Sub (size t - size (undefined :: t1 a))) where
+  f (Sub i k) = Sub (i + 1) (k (elementAt t i))
+
+data Sub a = Sub !Int a
+runSub :: Sub a -> a
+runSub (Sub _ x) = x
+
+
 {-# INLINABLE merge #-}
-merge ::  (Hot t (n + m), Hot t1 n, Hot t2 m, Ord a) => t1 a -> t2 a -> t a
+merge :: (Hot t (n + m), Hot t1 n, Hot t2 m, Ord a) => t1 a -> t2 a -> t a
 merge x y = runMerge $ unfold f (M 0 0 (elementAt x) (elementAt y)) where
   f (M i j g p k) | i == size x = take2
                   | j == size y = take1
