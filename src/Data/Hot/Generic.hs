77346ab152c042b7542ca6e8064f909826b0bfc4
Taras Serduke <taras.serduke@gmail.com>
2016-08-31 21:30:11 +0300

    #11 faster

--- src/Data/Hot/Generic.hs
+++ src/Data/Hot/Generic.hs
@@ -21,22 +21,25 @@ data Sub a b = Sub !Int (Int -> a) b
 buildSub :: Sub a (a -> r) -> Sub a r
 buildSub (Sub i f k) = Sub (i + 1) f (k (f i))
 
-runSub :: Sub f a -> a
+runSub :: Sub a r -> r
 runSub (Sub _ _ x) = x
 
 
 {-# INLINABLE merge #-}
 merge :: (Hot t (n + m), Hot t1 n, Hot t2 m, Ord a) => t1 a -> t2 a -> t a
-merge x y = runMerge $ unfold f (M 0 0 (elementAt x) (elementAt y)) where
-  f (M i j g p k) | i == size x = take2
-                  | j == size y = take1
-                  | p j > g i = take1
-                  | otherwise = take2
-                  where
-                    take1 = M (i + 1) j g p (k $ g i)
-                    take2 = M i (j + 1) g p (k $ p j)
+merge x y = runMerge $ unfold (buildMerge (size x) (size y)) (M 0 0 (elementAt x) (elementAt y))
 
-data Merge a b c = M !Int !Int a b c
+data Merge a b = M !Int !Int (Int -> a) (Int -> a) b
 
-runMerge :: Merge a b c -> c
+buildMerge :: (Ord a) => Int -> Int -> Merge a (a -> r) -> Merge a r
+buildMerge n m (M i j g p k)
+  | i == n = take2
+  | j == m = take1
+  | p j > g i = take1
+  | otherwise = take2
+  where
+    take1 = M (i + 1) j g p (k $ g i)
+    take2 = M i (j + 1) g p (k $ p j)
+
+runMerge :: Merge a r -> r
 runMerge (M _ _ _ _ x) = x
