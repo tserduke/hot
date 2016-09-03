cc244b0b9abc0324ab33c1c89c7444aa046c0900
Taras Serduke <taras.serduke@gmail.com>
2016-09-03 10:34:55 +0300

    #47

--- bench-compare/Compare.hs
+++ bench-compare/Compare.hs
@@ -3,8 +3,11 @@ module Main (main) where
 import Control.Monad (forM)
 import System.Directory (listDirectory)
 import System.FilePath ((</>), dropExtensions)
+import Text.PrettyPrint.Boxes ((<>), (//))
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Csv as C
+import qualified Data.Map as M
+import qualified Data.Set as S
 import qualified Data.Vector as V
 
 
@@ -12,18 +15,29 @@ main :: IO ()
 main = do
   files <- listDirectory "data"
   records <- forM files readData
-  print records
+  print $ map fst $ records
+  print $ combineRecords $ map snd records
   return ()
 
-readData :: FilePath -> IO (String, V.Vector (String, Double))
+
+type Record = (String, Double)
+
+combineRecords :: [V.Vector Record] -> ([String], [[Maybe Double]])
+combineRecords records = (names, map means records) where
+  means = map snd . M.toAscList . flip M.union namesMap . M.fromList . V.toList . V.map (fmap Just)
+  namesMap = M.fromAscList $ zip names (repeat Nothing)
+  names = S.toAscList $ S.unions $ map (S.fromList . V.toList . V.map fst) records
+
+
+readData :: FilePath -> IO (String, V.Vector Record)
 readData file = do
   content <- B.readFile ("data" </> file)
   return (dropExtensions file, decodeRecords content)
 
-decodeRecords :: B.ByteString -> V.Vector (String, Double)
+decodeRecords :: B.ByteString -> V.Vector Record
 decodeRecords = either error (V.map unRecord) . C.decode C.HasHeader
 
-newtype Record = Record { unRecord :: (String, Double) }
+newtype CsvRecord = CsvRecord { unRecord :: Record }
 
-instance C.FromRecord Record where
-  parseRecord x = fmap Record $ (,) <$> (x C..! 0) <*> (x C..! 1)
+instance C.FromRecord CsvRecord where
+  parseRecord x = fmap CsvRecord $ (,) <$> (x C..! 0) <*> (x C..! 1)
