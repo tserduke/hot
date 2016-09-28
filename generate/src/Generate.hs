94332f854ec10175a0659665361e07db22815817
Taras Serduke <taras.serduke@gmail.com>
2016-09-28 22:51:50 +0300

    #60 generate indentation refactoring

--- generate/src/Generate.hs
+++ generate/src/Generate.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Data.DoList (DoList, item, toList)
+import Data.DoList (DoList, fromList, item, toList)
 import Data.List (intercalate)
 
 
@@ -9,7 +9,7 @@ type Text = String
 main :: IO ()
 main = do
     let n = 10
-    writeFile "../instances/Data/Hot/Instances.hs" $ baseModule (pragmaFunc "INLINABLE" 1) n
+    writeFile "../instances/Data/Hot/Instances.hs" $ baseModule (pragmaFunc "INLINABLE") n
 
 
 baseModule :: (Text -> Text -> Line) -> Int -> Text
@@ -26,41 +26,45 @@ baseModule inline n = runLines $ do
     forN n (instanceFoldable inline)
 
 
-pragmaFunc :: Text -> Int -> Text -> Text -> Line
-pragmaFunc pragma ind func body = do
-    tab ind $ "{-#" +++ pragma +++ func +++ "#-}"
-    tab ind $ func +++ body
+pragmaFunc :: Text -> Text -> Text -> Line
+pragmaFunc pragma func body = do
+    item $ "{-#" +++ pragma +++ func +++ "#-}"
+    item $ func +++ body
 
 instanceHot, instanceFoldable :: (Text -> Text -> Line) -> Int -> Line
 instanceHot inline n = do
     line $ "instance HotClass" +++ show n +++ "where"
-    dataHot n
-    inline "unfold" $ "f z =" +++ concat (replicate n "f (") ++ "z Hot" ++ show n ++ replicate n ')'
-    inline "elementAt" $ hotMatching n +++ "= \\case"
-    forN n elementAtCase
-    tab 2 $ "n -> hotError" +++ show n +++ "\"elementAt\" n"
-    inline "mapAt" $ "f" +++ hotMatching n +++ "= \\case"
-    forN n (mapAtCase n)
-    tab 2 $ "n -> hotError" +++ show n +++ "\"mapAt\" n"
+    indent $ do
+        dataHot n
+        inline "unfold" $ "f z =" +++ concat (replicate n "f (") ++ "z Hot" ++ show n ++ replicate n ')'
+        inline "elementAt" $ hotMatching n +++ "= \\case"
+        indent $ do
+            forN n elementAtCase
+            item $ "n -> hotError" +++ show n +++ "\"elementAt\" n"
+        inline "mapAt" $ "f" +++ hotMatching n +++ "= \\case"
+        indent $ do
+            forN n (mapAtCase n)
+            item $ "n -> hotError" +++ show n +++ "\"mapAt\" n"
     ""
 
 instanceFoldable inline n = do
     line $ "instance Foldable (Hot" +++ show n ++ ") where"
-    pragmaFunc "INLINE" 1 "length" $ "_ =" +++ show n
-    inline "foldr" $ "f z" +++ hotMatching n +++ "= f x" ++
-        intercalate " (f x" (map show [1 .. n]) +++ "z" ++ replicate (n - 1) ')'
+    indent $ do
+        pragmaFunc "INLINE" "length" $ "_ =" +++ show n
+        inline "foldr" $ "f z" +++ hotMatching n +++ "= f x" ++
+            intercalate " (f x" (map show [1 .. n]) +++ "z" ++ replicate (n - 1) ')'
     ""
 
 
 dataHot, elementAtCase :: Int -> Line
 dataHot n = do
-    tab 1 $ "data Hot" +++ show n +++ "a =" +++ hotConstr n (const "!a")
-    tab 2 "deriving (Eq, Ord, Read, Show)"
+    item $ "data Hot" +++ show n +++ "a =" +++ hotConstr n (const "!a")
+    indent "deriving (Eq, Ord, Read, Show)"
 
-elementAtCase i = tab 2 $ show (i - 1) +++ "-> x" ++ show i
+elementAtCase i = item $ show (i - 1) +++ "-> x" ++ show i
 
 mapAtCase :: Int -> Int -> Line
-mapAtCase n i = tab 2 $ show (i - 1) +++ "->" +++ hotConstr n f where
+mapAtCase n i = item $ show (i - 1) +++ "->" +++ hotConstr n f where
     f j = if j == i then "(f x" ++ show j ++ ")" else "x" ++ show j
 
 hotMatching :: Int -> Text
@@ -76,8 +80,8 @@ forN n f = foldr1 (>>) $ map f [1 .. n]
 (+++) :: Text -> Text -> Text
 x +++ y = x ++ " " ++ y
 
-tab :: Int -> Text -> Line
-tab n x = line $ concat (replicate n "  ") ++ x
+indent :: Line -> Line
+indent = fromList . map ("    " ++) . toList
 
 
 type Line = DoList Text
